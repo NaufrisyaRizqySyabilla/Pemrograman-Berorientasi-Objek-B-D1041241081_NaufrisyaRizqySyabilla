d342ca953b178d5a2a940ba999ec178cf60dd9a0
NaufrisyaRizqySyabilla <d1041241081@student.untan.ac.id>
2026-02-05 15:39:35 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama_Latihan.java
@@ -0,0 +1,14 @@
+public class KodePertama_Latihan {
+    public static void main(String[] args){
+        System.out.println("Nama        : Naufrisya Rizqy Syabilla ");
+        System.out.println("NIM         : D1041241081");
+        System.out.println("Semester    : 4 ");
+        System.out.println("Prodi       : Informatika"); 
+} 
+
+}
+/*
+Naufrisya Rizqy Syabilla
+D1041241081
+Kamis, 5 Februari 2025
+ */
