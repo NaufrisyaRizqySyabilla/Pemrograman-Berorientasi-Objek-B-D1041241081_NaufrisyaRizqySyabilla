4d0c12ad37ddb2d5d8c8997f3a56185e55d948af
NaufrisyaRizqySyabilla <d1041241081@student.untan.ac.id>
2026-02-12 14:29:26 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,13 @@
+package Pertemuan3;
+public class p022WorkingVariable {
+
+    public static void main(String[] args) {
+
+        int angka = 10;
+        System.out.println("Nilai awal angka: " + angka);
+        angka = 25;
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+
+}
+
