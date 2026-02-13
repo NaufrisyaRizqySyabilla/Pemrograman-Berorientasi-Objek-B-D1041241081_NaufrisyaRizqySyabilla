d1a570fbba88d85023991874d56fc50cf65bbd75
NaufrisyaRizqySyabilla <d1041241081@student.untan.ac.id>
2026-02-13 09:04:23 +0700

    Pertemuan4

---
+++ Pertemuan4/p042OperatorAritmatika.java
@@ -0,0 +1,34 @@
+package Pertemuan3;
+
+public class p042OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println();
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        double hasil = (double) a / b;
+        System.out.println("a / b (desimal) = " + hasil);
+
+    }
+
+}
+/*
+Naufrisya Rizqy Syabilla
+D1041241081
+Jumat, 13 Februari 2025
+ */
+    
+
+
+    
+
+
