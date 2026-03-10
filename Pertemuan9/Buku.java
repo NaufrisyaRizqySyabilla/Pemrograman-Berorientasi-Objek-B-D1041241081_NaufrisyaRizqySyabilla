272aeb5fbb75324cefce9d3914604eb4ab8e735d
NaufrisyaRizqySyabilla <d1041241081@student.untan.ac.id>
2026-03-10 17:34:50 +0700

    Pertemuan9

---
+++ Pertemuan9/Buku.java
@@ -0,0 +1,37 @@
+package Pertemuan9;
+
+class Buku {
+
+    String judul;
+    String pengarang;
+    int harga;
+
+    void tampilkanInfo() {
+        System.out.println("=== INFORMASI BUKU ===");
+        System.out.println("Judul      : " + judul);
+        System.out.println("Pengarang  : " + pengarang);
+        System.out.println("Harga      : Rp " + harga);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Buku buku1 = new Buku();
+        buku1.judul = "Pemrograman Java";
+        buku1.pengarang = "Budi Raharjo";
+        buku1.harga = 125000;
+
+        Buku buku2 = new Buku();
+        buku2.judul = "Algoritma dan Struktur Data";
+        buku2.pengarang = "Rinaldi Munir";
+        buku2.harga = 150000;
+
+        buku1.tampilkanInfo();
+        buku2.tampilkanInfo();
+    }
+}
+/*
+Naufrisya Rizqy Syabilla
+D1041241081
+senin, 9 Maret 2026
+*/
